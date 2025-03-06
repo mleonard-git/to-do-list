21affe6555a81762d32ad35d36923f47eb51147c
Mike Leonard <mike.leonard@uk.ey.com>
2025-03-06 20:56:43 +0000

    Add entity fields and data types

--- src/main/java/com/qa/tdl/java/model/TaskModel.java
+++ src/main/java/com/qa/tdl/java/model/TaskModel.java
@@ -1,4 +1,11 @@
 package com.qa.tdl.java.model;
 
 public class TaskModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String task;
+    private Boolean isTaskCompleted;
+
 }
