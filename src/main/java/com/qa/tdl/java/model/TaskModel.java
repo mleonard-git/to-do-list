0db27b5d76c54b5770eda991593e15e467e07df0
Mike Leonard <mike.leonard@uk.ey.com>
2025-03-06 20:57:32 +0000

    Add entity and data annotations to model class

--- src/main/java/com/qa/tdl/java/model/TaskModel.java
+++ src/main/java/com/qa/tdl/java/model/TaskModel.java
@@ -1,5 +1,13 @@
 package com.qa.tdl.java.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class TaskModel {
 
     @Id
