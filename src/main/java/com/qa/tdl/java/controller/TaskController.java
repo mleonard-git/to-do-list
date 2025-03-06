786742592e7a9780965a059f892e313d87a7fb81
Mike Leonard <mike.leonard@uk.ey.com>
2025-03-06 21:55:08 +0000

    Add Post request method to create new task

--- src/main/java/com/qa/tdl/java/controller/TaskController.java
+++ src/main/java/com/qa/tdl/java/controller/TaskController.java
@@ -1,4 +1,26 @@
 package com.qa.tdl.java.controller;
 
+import com.qa.tdl.java.model.Task;
+import com.qa.tdl.java.service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping(value = "/")
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        return ResponseEntity.ok(taskService.createNewTask(task));
+    }
+
 }
