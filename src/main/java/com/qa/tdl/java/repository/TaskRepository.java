0a39ea3fa2b9127a1515149883573bfcf11afd29
Mike Leonard <mike.leonard@uk.ey.com>
2025-03-06 21:53:18 +0000

    Add Repository logic to extend JPARepository

--- src/main/java/com/qa/tdl/java/repository/TaskRepository.java
+++ src/main/java/com/qa/tdl/java/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.qa.tdl.java.repository;
 
-public class TaskRepository {
+import com.qa.tdl.java.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
